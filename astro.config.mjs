c2352ed30da5b0063111c5f2d589403cab2e5d0d
jesseagleboy <jslomowitz@gmail.com>
2025-01-15 21:01:59 -0500

    added integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,34 @@
 import { defineConfig } from "astro/config";
 import { imageService } from "@unpic/astro/service";
 
+import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig( {
-    image: {
-        // experimentalLayout: 'responsive',
-        service: imageService(),
-    },
+  site: 'https://jesse-photo-website.netlify.app/',
+	image: {
+		// experimentalLayout: 'responsive',
+    service: imageService( {
+      placeholder: 'blurhash',
+    }),
+	},
+
 	experimental: {
 		responsiveImages: true,
 		svg: true,
 	},
+
+	integrations: [
+		sitemap(),
+		tailwind(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+	],
 });
