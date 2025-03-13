e617ebd3f2f12b48f55e91f02e1d9806d1a8725d
jesseagleboy <jslomowitz@gmail.com>
2025-03-13 17:48:14 -0400

    add manifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
-
 import partytown from "@astrojs/partytown";
-
 import svelte from "@astrojs/svelte";
-
 import robots from "astro-robots";
-
 import tailwindcss from "@tailwindcss/vite";
+import webmanifest from "astro-webmanifest";
 
 // https://astro.build/config
 export default defineConfig({
@@ -36,6 +32,16 @@ export default defineConfig({
 		}),
 		svelte(),
 		robots(),
+		webmanifest({
+			name: "Disney Fireworks by Jesse Slomowitz",
+			short_name: "Disney Fireworks",
+			icon: "src/assets/jslogo-manifest.svg",
+			description: "A collection of Disney fireworks shows.",
+			start_url: "/",
+			theme_color: "#3367D6",
+			background_color: "#3367D6",
+			display: "standalone",
+		}),
 	],
 
 	vite: {
